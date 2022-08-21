c456a8feb6b3e598aa5778cc4c3be4b294aa362e
Denis Yurovsky <moon.wave578@gmail.com>
2022-08-21 20:32:57 +0300

    v 0.1

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 
 const message = document.querySelector('.message');
 
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let scoreValue = 20;
+
+console.log(document.querySelector('.score'));
+
 const correctNumber = 'Correct Number!';
 
 const checkButton = document.querySelector('.check');
@@ -9,7 +15,30 @@ const checkButton = document.querySelector('.check');
 checkButton.addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
 
-  if (!guess) {
-    message.textContent = 'Provide a number!';
+  if (scoreValue > 1) {
+    if (!guess) {
+      message.textContent = 'Provide a number!';
+    }
+    if (guess > 20 || guess < 1) {
+      message.textContent = 'Provide a number between 1 to 20';
+    }
+
+    if (guess > secretNumber) {
+      message.textContent = 'Too high!';
+      scoreValue--;
+      document.querySelector('.score').textContent = scoreValue;
+    }
+
+    if (guess < secretNumber) {
+      message.textContent = 'Too low!';
+      scoreValue--;
+      document.querySelector('.score').textContent = scoreValue;
+    }
+
+    if (guess == secretNumber) {
+      message.textContent = 'Correct!';
+    }
+  } else {
+    message.textContent = 'You lost!';
   }
 });
